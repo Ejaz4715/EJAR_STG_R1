518bbd238359da5f34cd74df8576d0a42de170d1
Ejaz4715 <ejazmd15@hotmail.com>
2024-05-12 10:29:55 +0300

    Change lessor TC-03-05

--- src/main/java/sa/ejar/web/pages/ChangeLessor_LessorRepPage.java
+++ src/main/java/sa/ejar/web/pages/ChangeLessor_LessorRepPage.java
@@ -1,12 +1,15 @@
 package sa.ejar.web.pages;
 
+import com.testcrew.api.UnirestAPI;
 import com.testcrew.manager.TestDataManager;
 import com.testcrew.web.Browser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import sa.ejar.web.objects.ChangeLessor_LessorRepPageObjects;
+import sa.ejar.web.objects.CommonMethodsPageObjects;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -51,4 +54,44 @@ public class ChangeLessor_LessorRepPage {
         TestDataManager.writeDependantGlobalTestData("ChangeLessor");
     }
 
+    public void clickOnPropertiesTab() {
+        Browser.waitUntilVisibilityOfElement(ChangeLessor_LessorRepPageObjects.PropertyTab(), 40);
+        Browser.click(ChangeLessor_LessorRepPageObjects.PropertyTab());
+        Browser.click(By.xpath("//span[contains(text(),'عرض جميع وثائق الملكية')]"));
+    }
+
+    public void clickOnViewOwnerShipDocumentsOption() {
+        Browser.waitUntilVisibilityOfElement(ChangeLessor_LessorRepPageObjects.ViewOwnerShipDocumentsOption(), 40);
+        Browser.click(ChangeLessor_LessorRepPageObjects.ViewOwnerShipDocumentsOption());
+    }
+
+    public void verifyOwnerShipDocumentPageIsDisplayed() {
+        Browser.waitUntilVisibilityOfElement(ChangeLessor_LessorRepPageObjects.OwnerShipDocumentPageTitle(), 40);
+        Assert.assertTrue(Browser.isElementDisabled(ChangeLessor_LessorRepPageObjects.OwnerShipDocumentPageTitle()));
+        logger.addScreenshot("");
+    }
+
+    public void enterOwnerShipDocumentNumberInSearchField(String deedNumber) {
+        Browser.waitUntilVisibilityOfElement(ChangeLessor_LessorRepPageObjects.OwnershipDocumentSearchField(), 40);
+        Browser.setText(ChangeLessor_LessorRepPageObjects.OwnershipDocumentSearchField(), deedNumber);
+    }
+
+    /**
+     * Verify the searched deed number is displayed on OwnerShip Documents page
+     * @param expectedDeedNumber - Contract number to be validated
+     */
+    public void verifySearchedOwnershipDocumentIsDisplayed(String expectedDeedNumber) {
+        Browser.waitForSeconds(1);
+        if (Browser.isElementDisplayed(CommonMethodsPageObjects.SearchedDeedNumber())) {
+            String actualDeedNumber = Browser.getText(CommonMethodsPageObjects.SearchedDeedNumber());
+            boolean status = false;
+            if (actualDeedNumber.contains(expectedDeedNumber)) {
+                status = true;
+            }
+            Assert.assertTrue(status, "Searched deed number is not displayed");
+            logger.addScreenshot("Searched deed number is displayed");
+        } else {
+            logger.addScreenshot("Searched deed number is not displayed");
+        }
+    }
 }
--- src/test/java/sa/ejar/web/login/ChangeLessor_LessorRep.java
+++ src/test/java/sa/ejar/web/login/ChangeLessor_LessorRep.java
@@ -2,6 +2,7 @@ package sa.ejar.web.login;
 
 import org.testng.annotations.Test;
 import sa.ejar.web.base.NHCWebTest;
+import sa.ejar.web.objects.ChangeLessor_LessorRepPageObjects;
 import sa.ejar.web.pages.CommonMethodsPage;
 
 import java.util.Map;
@@ -69,4 +70,70 @@ public class ChangeLessor_LessorRep extends NHCWebTest {
         app.changeLessor_lessorRepPage.getContractVersionFromPDF(data.get("Contract_Number"));
     }
 
+    @Test(dataProvider = "testDataProvider")
+    public void TC_03_ChangeLessor_LessorRep(Map<String, String> data) throws Exception {
+        logger.info("Step 00: Test Data : " + data.toString());
+        app.openApplication(data);
+        logger.info("Step 01: Login to Application Enter Username, Enter Password, click Login");
+        app.loginPage.enterUsername(data.get("Username"));
+        app.loginPage.enterPassword(data.get("Password"));
+        app.loginPage.clickLogin();
+        app.loginPage.enterVerificationCode(data.get("OTP"));
+        app.loginPage.closeExploreEjarPopUp();
+        CommonMethodsPage.changeUserRole("مدير مكتب الوساطة");
+        logger.info("Step 02: Click on \" العقارات \" tab");
+        app.changeLessor_lessorRepPage.clickOnPropertiesTab();
+        logger.info("Step 03: Click on \"عرض جميع وثائق الملكية\"");
+        app.changeLessor_lessorRepPage.clickOnViewOwnerShipDocumentsOption();
+        logger.info("Step 04: Verify \"وثائق الملكية\" page is displayed with all the ownerhsip documents");
+        app.changeLessor_lessorRepPage.verifyOwnerShipDocumentPageIsDisplayed();
+    }
+
+    @Test(dataProvider = "testDataProvider")
+    public void TC_04_ChangeLessor_LessorRep(Map<String, String> data) throws Exception {
+        logger.info("Step 00: Test Data : " + data.toString());
+        app.openApplication(data);
+        logger.info("Step 01: Login to Application Enter Username, Enter Password, click Login");
+        app.loginPage.enterUsername(data.get("Username"));
+        app.loginPage.enterPassword(data.get("Password"));
+        app.loginPage.clickLogin();
+        app.loginPage.enterVerificationCode(data.get("OTP"));
+        app.loginPage.closeExploreEjarPopUp();
+        CommonMethodsPage.changeUserRole("مدير مكتب الوساطة");
+        logger.info("Step 02: Click on \" العقارات \" tab");
+        app.changeLessor_lessorRepPage.clickOnPropertiesTab();
+        logger.info("Step 03: Click on \"عرض جميع وثائق الملكية\"");
+        app.changeLessor_lessorRepPage.clickOnViewOwnerShipDocumentsOption();
+        logger.info("Step 04: Click on filter icon");
+        CommonMethodsPage.clickFilterBtn();
+        logger.info("Step 05: Enter owner ship document number in \"رقم وثيقة الملكية\" ");
+        app.changeLessor_lessorRepPage.enterOwnerShipDocumentNumberInSearchField(data.get("Deed_Number"));
+        app.changeLessor_lessorRepPage.verifySearchedOwnershipDocumentIsDisplayed(data.get("Deed_Number"));
+    }
+
+    @Test(dataProvider = "testDataProvider")
+    public void TC_05_ChangeLessor_LessorRep(Map<String, String> data) throws Exception {
+        logger.info("Step 00: Test Data : " + data.toString());
+        app.openApplication(data);
+        logger.info("Step 01: Login to Application Enter Username, Enter Password, click Login");
+        app.loginPage.enterUsername(data.get("Username"));
+        app.loginPage.enterPassword(data.get("Password"));
+        app.loginPage.clickLogin();
+        app.loginPage.enterVerificationCode(data.get("OTP"));
+        app.loginPage.closeExploreEjarPopUp();
+        CommonMethodsPage.changeUserRole("مدير مكتب الوساطة");
+        logger.info("Step 02: Click on \" العقارات \" tab");
+        app.changeLessor_lessorRepPage.clickOnPropertiesTab();
+        logger.info("Step 03: Click on \"عرض جميع وثائق الملكية\"");
+        app.changeLessor_lessorRepPage.clickOnViewOwnerShipDocumentsOption();
+        logger.info("Step 04: Click on filter icon");
+        CommonMethodsPage.clickFilterBtn();
+        logger.info("Step 05: Enter owner ship document number in \"رقم وثيقة الملكية\" ");
+        app.changeLessor_lessorRepPage.enterOwnerShipDocumentNumberInSearchField(data.get("Deed_Number"));
+        logger.info("Step 06: Click on filter icon");
+        CommonMethodsPage.clickOnKebabMenuButton();
+        logger.info("Step 07: Select \"تغيير ممثل المؤجر\" option from the menu");
+        CommonMethodsPage.ClickOnKebabMenuOption("تغيير ممثل المؤجر");
+        CommonMethodsPage.verifyValueIsDisplayed("تغيير ممثل المؤجر", ChangeLessor_LessorRepPageObjects.ChangeLessorRepPageTitle());
+    }
 }
